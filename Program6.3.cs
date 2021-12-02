3367e80d0b3aa47e70f73e00958312c574d161ec
Irunka1306 <92487724+Irunka1306@users.noreply.github.com>
2021-12-02 13:53:33 +0200

    Add files via upload

---
+++ Program6.3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab63
+{
+    public class Program
+    {
+       public static int CountOfOdd(int[] arr)
+        {
+            int result = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+       public static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-20, 34);
+                Console.WriteLine(array[i] + "\t");
+            }
+
+            Console.WriteLine("Result = " + CountOfOdd(array));
+        }
+    }
+}
